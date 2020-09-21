9cd6ae8a7be5cbfc59ab4d2b3fbd800c2275f31b
SamRamdhan <69948351+SamRamdhan@users.noreply.github.com>
2020-09-22 02:05:50 +0800

    Update reporting.py

--- tg_bot/modules/reporting.py
+++ tg_bot/modules/reporting.py
@@ -25,11 +25,11 @@ def report_setting(bot: Bot, update: Update, args: List[str]):
         if len(args) >= 1:
             if args[0] in ("yes", "on"):
                 sql.set_user_setting(chat.id, True)
-                msg.reply_text("Turned on reporting! You'll be notified whenever anyone reports something.")
+                msg.reply_text("Mengaktifkan laporan! Anda akan diberi tahu jika ada laporan.")
 
             elif args[0] in ("no", "off"):
                 sql.set_user_setting(chat.id, False)
-                msg.reply_text("Turned off reporting! You wont get any reports.")
+                msg.reply_text("Menonaktifkan laporan! Anda tidak akan mendapat laporan apapun.")
         else:
             msg.reply_text(f"Your current report preference is: `{sql.user_should_report(chat.id)}`",
                            parse_mode=ParseMode.MARKDOWN)
