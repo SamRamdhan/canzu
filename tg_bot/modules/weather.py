542cbad0043926a5242fd22e2550a84579589733
SamRamdhan <69948351+SamRamdhan@users.noreply.github.com>
2020-09-22 09:34:56 +0800

    Update weather.py

--- tg_bot/modules/weather.py
+++ tg_bot/modules/weather.py
@@ -54,7 +54,7 @@ def weather(bot, update, args):
         status += theweather._detailed_status
                         
 
-        update.message.reply_text("Today in {} is being {}, around {}°C.\n".format(thelocation,
+        update.message.reply_text("Cuaca di {} sedang {}, suhu sekitar {}°C.\n".format(thelocation,
                 status, temperature))
 
     except pyowm.exceptions.not_found_error.NotFoundError:
